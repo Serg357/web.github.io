ac78c146d04b01f279b2e40f636e27cefb4c6c3f
Сергей <hodasevi4@gmail.com>
2020-12-02 19:29:38 +0300

    создан начальный код-заглушка

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let btnDefault = document.querySelector('.btn-default');
+let intro = document.querySelector('.intro');
+
+btnDefault.addEventListener('click', () => { alert('Клик по кнопке!'); });
+
+intro.style.border = '5px dashed gold';
+document.body.style.backgroundColor = 'lightgray';
